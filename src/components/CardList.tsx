efa6895e8406cc155bbef07e0e3219a8289db4b8
sarahgm <sarah.gosmann@reservix.de>
2022-09-27 11:12:34 +0200

    add CardList as component and retrun data

---
+++ src/components/CardList.tsx
@@ -0,0 +1,26 @@
+import { Headline, List } from '@marigold/components';
+
+export interface CardListProps {
+  headline: string;
+  data: any;
+}
+
+const CardList = ({ headline, data }: CardListProps) => {
+  console.log(data);
+  if (data?.length != 0) {
+    return (
+      <>
+        <Headline level="2">{headline}</Headline>
+        <List>
+          {data?.map((item: any) => (
+            <List.Item key={item}>{item}</List.Item>
+          ))}
+        </List>
+      </>
+    );
+  } else {
+    return null;
+  }
+};
+
+export default CardList;
