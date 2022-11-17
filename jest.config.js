71a2489fdad840b19303914fbd8d88e3b61870fa
sarahgm <sarah.gosmann@reservix.de>
2022-11-17 14:55:01 +0100

    chore: add testing library

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const path = require('node:path');
+const createConfig = require('@marigold/jest-config');
+
+module.exports = createConfig({
+  setupFilesAfterEnv: [path.resolve(__dirname, 'jest.setup.ts')],
+  collectCoverageFrom: ['!**/.parcel-cache/**', '!**/dist/**'],
+  moduleNameMapper: {},
+});
