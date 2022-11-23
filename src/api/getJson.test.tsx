793d173a95ffa1111cf81238093bf6cc58f6a2cd
sarahgm <sarah.gosmann@reservix.de>
2022-11-23 10:15:05 +0100

    get json tests

--- src/api/getJson.test.tsx
+++ src/api/getJson.test.tsx
@@ -6,27 +6,44 @@ import { Film, useFilm } from '~/components';
 import { getIdFromUrl } from './getIdFromUrl';
 import { getJson } from './getJson';
 
-nock('https://swapi.py4e.com').get('/api/films/1/').reply(
-  200,
-  {
-    title: 'A new Hope',
-    url: 'https://swapi.py4e.com/api/films/1/',
-  },
-  { 'Access-Control-Allow-Origin': '*' }
-);
+// with that the error message from nock isn't shown
+beforeEach(() => {
+  jest.spyOn(console, 'error').mockImplementation(() => {});
+});
 
 test('get json from and fetch url', async () => {
+  nock('https://swapi.py4e.com').get('/api/films/1/').reply(
+    200,
+    {
+      title: 'A new Hope',
+      url: 'https://swapi.py4e.com/api/films/1/',
+    },
+    { 'Access-Control-Allow-Origin': '*' }
+  );
+
   const url = 'https://swapi.py4e.com/api/films/1/';
   expect(await getJson(url));
 });
 
-test('get json error', async () => {
+test('error on response', async () => {
+  nock('https://swapi.py4e.com')
+    .get('/api/films')
+    .reply(500, 'Network  failed', { 'Access-Control-Allow-Origin': '*' });
   const url = 'https://swapi.py4e.com/api/films';
 
-  try {
-    return expect(await getJson(url)).rejects.toThrow();
-  } catch (e) {
-    expect(e.message).toEqual('Network request failed');
-  }
   expect.assertions(1);
+  return getJson(url).catch(e => {
+    expect(e.message).toMatchInlineSnapshot(`"Network response was not ok"`);
+  });
+});
+
+test('error on request', () => {
+  nock('https://swapi.py4e.com')
+    .get('/api/films')
+    .replyWithError('Network  failed');
+  const url = 'https://swapi.py4e.com/api/films';
+  expect.assertions(1);
+  return getJson(url).catch(e => {
+    expect(e.message).toMatchInlineSnapshot(`"Network request failed"`);
+  });
 });
