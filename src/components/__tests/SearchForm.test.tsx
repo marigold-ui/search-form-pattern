2cd184221b08dce166dacc4df7f6d6a9175aade2
sarahgm <sarah.gosmann@reservix.de>
2022-11-24 11:17:01 +0100

    saves

--- src/components/__tests/SearchForm.test.tsx
+++ src/components/__tests/SearchForm.test.tsx
@@ -2,35 +2,41 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { SearchForm } from '../SearchForm';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 test('renders search from', () => {
   render(
-    <BrowserRouter>
+    <MemoryRouter>
       <SearchForm />
-    </BrowserRouter>
+    </MemoryRouter>
   );
 
   const search = screen.getByLabelText('Search');
   expect(search).toBeInTheDocument();
 });
 
-test('invokes submit handler with search name and submits form', async () => {
+test('text can be written in search input', async () => {
   const searchName = userEvent.setup();
 
-  const spy = jest.fn();
-
   render(
-    <BrowserRouter>
-      <SearchForm onSubmit={spy} />
-    </BrowserRouter>
+    <MemoryRouter>
+      <SearchForm />
+    </MemoryRouter>
   );
 
   const search = screen.getByPlaceholderText('Search...');
-
   await searchName.type(search, 'Luke');
-  fireEvent.submit(screen.getByRole('form'));
+  expect(search).toHaveValue('Luke');
+});
 
-  // expect(spy.mock.calls).toMatchInlineSnapshot();
+test('search submit with click', () => {
+  const spy = jest.fn();
+  render(
+    <MemoryRouter>
+      <SearchForm onSubmit={spy} />
+    </MemoryRouter>
+  );
+
+  fireEvent.submit(screen.getByRole('form'));
   expect(spy).toHaveBeenCalled();
 });
